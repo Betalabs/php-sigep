860edf0003a260e71f2d1ef95844f3b41559e5f6
marcelo <marcelo.gomes@betalabs.com.br>
2021-03-08 13:53:46 -0300

    sigep

--- src/PhpSigep/Services/Real/FecharPreListaDePostagem.php
+++ src/PhpSigep/Services/Real/FecharPreListaDePostagem.php
@@ -66,10 +66,10 @@ class FecharPreListaDePostagem
             if ($e instanceof \SoapFault) {
                 $result->setIsSoapFault(true);
                 $result->setErrorCode($e->getCode());
-                $result->setErrorMsg(SoapClientFactory::convertEncoding($e->getMessage()));
+                $result->setErrorMsg('soap fault ' . print_r($xml, true)  .   SoapClientFactory::convertEncoding($e->getMessage()));
             } else {
                 $result->setErrorCode($e->getCode());
-                $result->setErrorMsg($e->getMessage());
+                $result->setErrorMsg('xml ' . print_r($xml, true)  . $e->getMessage());
             }
         }
         
